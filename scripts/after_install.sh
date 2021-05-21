94705fd0810f7b3d573e7c5090dd5347bbc6d71d
Shitikov Kirill <k.shitikov@itransition.com>
2021-05-22 00:54:59 +0300

    fix scripts

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -7,6 +7,7 @@ chmod -R 777 $MAGE_ROOT
 echo "Restart nginx"
 sudo service nginx restart
 
+rm -rf "${MAGE_ROOT}/app/etc"
 if [ ! -f "${MAGE_ROOT}/app/etc" ]; then
     echo "Add etc link from efs to ${MAGE_ROOT}"
     ln -s /mnt/efs/etc/ "${MAGE_ROOT}/app/"
