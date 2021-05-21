438694cb85aecffd04088efca1d7cda35ebe5c3d
Shitikov Kirill <k.shitikov@itransition.com>
2021-05-22 00:06:08 +0300

    Add link to etc to magento folder

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,4 +1,11 @@
 env
 MAGE_ROOT="/var/www/html/magento"
-sudo service nginx restart
+
+echo "Fix permission"
 chmod -R 777 $MAGE_ROOT
+
+echo "Restart nginx"
+sudo service nginx restart
+
+echo "Add link from efs to magento folder"
+ln -s /mnt/efs/etc/ /var/www/html/magento/
