2c7eec28b1bed7af2cd4e03b9ee7f5705c49c850
Shitikov Kirill <k.shitikov@itransition.com>
2021-05-22 00:27:54 +0300

    Fix etc

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -7,5 +7,7 @@ chmod -R 777 $MAGE_ROOT
 echo "Restart nginx"
 sudo service nginx restart
 
-echo "Add link from efs to magento folder"
-ln -s /mnt/efs/etc/ /var/www/html/magento/app/
+if [ ! -f "${MAGE_ROOT}/app/etc" ]; then
+    echo "Add link from efs to magento folder"
+    ln -s /mnt/efs/etc/ /var/www/html/magento/app/
+fi
