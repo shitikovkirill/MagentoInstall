ed611e464b95332713001e06fad725d2ce55eada
Shitikov Kirill <k.shitikov@itransition.com>
2021-05-22 19:17:44 +0300

    Add media to efs

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -18,5 +18,14 @@ if [ ! -f "${MAGE_ROOT}/app/etc" ]; then
     ln -s "${EFS_ROOT}/etc/" "${MAGE_ROOT}/app/"
 fi
 
+echo "Create ${EFS_ROOT}/media/ folder"
+mkdir -p "${EFS_ROOT}/media/"
+echo "Delete ${MAGE_ROOT}/pub/media/ folder"
+rm -rf "${MAGE_ROOT}/pub/media/"
+if [ ! -f "${MAGE_ROOT}/pub/media/" ]; then
+    echo "Add etc link from efs to ${MAGE_ROOT}"
+    ln -s "${EFS_ROOT}/media/" "${MAGE_ROOT}/pub/media/"
+fi
+
 php "${MAGE_ROOT}/bin/magento" setup:upgrade
 php "${MAGE_ROOT}/bin/magento" setup:di:compile
