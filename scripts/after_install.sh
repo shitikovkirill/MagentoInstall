90daa23ff8bc00c7ac980da71eea1a8eb300521d
Shitikov Kirill <k.shitikov@itransition.com>
2021-05-23 20:54:35 +0300

    Fix permission

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -24,11 +24,18 @@ if [ ! -f "${MAGE_ROOT}/pub/media/" ]; then
     ln -s "${EFS_ROOT}/media/" "${MAGE_ROOT}/pub/"
 fi
 
+echo "Fix permission"
+find "${MAGE_ROOT}" -type f -exec chmod 664 {} \;
+find "${MAGE_ROOT}" -type d -exec chmod 775 {} \;
+find "${MAGE_ROOT}/var" "${MAGE_ROOT}/pub/static" "${MAGE_ROOT}/pub/media" "${MAGE_ROOT}app/etc" -type f -exec chmod g+w {} \;
+find "${MAGE_ROOT}/var" "${MAGE_ROOT}/pub/static" "${MAGE_ROOT}/pub/media" "${MAGE_ROOT}app/etc" -type d -exec chmod g+ws {} \;
+find "${MAGE_ROOT}/var" -type d -exec chmod 777 {} \;
+chmod u+x "${MAGE_ROOT}/bin/magento"
+chown -R magento:magento "${MAGE_ROOT}"
+chmod -R g+w "${MAGE_ROOT}/var/"
+chmod -R g+w "${MAGE_ROOT}/pub/media/"
+
 php "${MAGE_ROOT}/bin/magento" setup:upgrade
 php "${MAGE_ROOT}/bin/magento" setup:di:compile
-
-echo "Fix permission"
-sudo chmod -R 777 $MAGE_ROOT
-sudo rm -rf "${MAGE_ROOT}/var/page_cache"
 
 echo "Done"
