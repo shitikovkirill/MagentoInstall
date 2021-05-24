df96d474519f5a79b3fe56cd0e493da767fc4ad5
Shitikov Kirill <k.shitikov@itransition.com>
2021-05-24 20:56:16 +0300

    Cling cleanup

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,6 +1,5 @@
-set +e
-
+set +ex
 MAGE_ROOT="/var/www/html/magento"
-echo "Force code cleanup before AWS Deploy process"
 
+echo "Force code cleanup before AWS Deploy process"
 rm -rf ${MAGE_ROOT}/*
--- scripts/renew_etc.sh
+++ scripts/renew_etc.sh
@@ -1,5 +1,6 @@
 set -ex
 EFS_ROOT="/mnt/efs"
+MAGE_ROOT="/var/www/html/magento"
 
 echo "Renew etc"
-yes | cp -rf "./app/etc/*" "${EFS_ROOT}/etc/"
+yes | cp -rf ${MAGE_ROOT}/app/etc/* "${EFS_ROOT}/etc/"
